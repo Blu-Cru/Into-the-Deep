0fbf6b23199148ba9c061d8ae1e9841727e0bc4e
Kevin Dong <107311558+kevinndongg@users.noreply.github.com>
2024-11-29 22:50:32 -0500

    WE READY!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/pathbase/sample/SampleLiftingPath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/pathbase/sample/SampleLiftingPath.java
@@ -7,14 +7,29 @@ import org.firstinspires.ftc.teamcode.blucru.common.commandbase.sample.SampleBac
 import org.firstinspires.ftc.teamcode.blucru.common.path.PIDPathBuilder;
 
 public class SampleLiftingPath extends PIDPathBuilder {
-    public SampleLiftingPath(long waitBeforeLiftMillis) {
+    public SampleLiftingPath(boolean useSubsystems, long waitBeforeLiftMillis) {
         super();
-        this.setPower(0.3)
-                .schedule(new SequentialCommandGroup(
-                        new WaitCommand(waitBeforeLiftMillis),
-                        new SampleBackHighCommand()
-                ))
-                .addMappedPoint(-53, -53, 45, 5)
+        this.setPower(0.3);
+
+        if(useSubsystems) {
+            this.schedule(new SequentialCommandGroup(
+                    new WaitCommand(waitBeforeLiftMillis),
+                    new SampleBackHighCommand()
+            ));
+        }
+        this.addMappedPoint(-53, -53, 45, 5)
                 .waitMillis(400);
+    }
+
+    public SampleLiftingPath(long waitBeforeLiftMillis) {
+        this(true, waitBeforeLiftMillis);
+    }
+
+    public SampleLiftingPath() {
+        this(true,0);
+    }
+
+    public SampleLiftingPath(boolean useSubsystems) {
+        this(useSubsystems,0);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmode/test/AprilTagDriveTest.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.blucru.opmode.test;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.sfdev.assembly.state.StateMachine;
+import com.sfdev.assembly.state.StateMachineBuilder;
+
+import org.firstinspires.ftc.teamcode.blucru.opmode.BluLinearOpMode;
+
+@TeleOp(name = "AprilTag Drive Test", group = "test")
+public class AprilTagDriveTest extends BluLinearOpMode {
+    enum State {
+        DRIVER_CONTROL,
+        DRIVE_TO_DEPOSIT,
+    }
+    State state = State.DRIVER_CONTROL;
+    StateMachine sm;
+
+    @Override
+    public void initialize() {
+        addDrivetrain();
+        addCVMaster();
+        enableFTCDashboard();
+
+        sm = new StateMachineBuilder()
+                .state(State.DRIVER_CONTROL)
+                .transition(() -> stickyG1.a, State.DRIVE_TO_DEPOSIT, () -> {
+                    dt.pidTo(new Pose2d(-53, -53, Math.PI/4));
+                })
+                .loop(() -> {
+                    dt.teleOpDrive(gamepad1);
+
+                    if(stickyG1.right_stick_button) {
+                        dt.setHeading(Math.PI/2);
+                    }
+                })
+                .state(State.DRIVE_TO_DEPOSIT)
+                .transition(() -> stickyG1.a, State.DRIVER_CONTROL, () -> {
+                    dt.idle();
+                })
+                .build();
+
+        sm.start();
+    }
+
+    @Override
+    public void onStart() {
+        cvMaster.detectTag();
+    }
+
+    @Override
+    public void periodic() {
+        sm.update();
+        dt.updateAprilTags(cvMaster.tagDetector);
+        dt.drawPose();
+    }
+
+    @Override
+    public void telemetry() {
+        telemetry.addData("State", state);
+    }
+}
