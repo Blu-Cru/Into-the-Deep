f5705edce03e0af47dda30af16637ac3be398d9f
Kevin Dong <107311558+kevinndongg@users.noreply.github.com>
2024-12-05 18:59:09 -0500

    full kinematics done?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/boxtube/BoxtubeKinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/boxtube/BoxtubeKinematics.java
@@ -1,7 +1,76 @@
 package org.firstinspires.ftc.teamcode.blucru.common.subsystems.boxtube;
 
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.ejml.data.DMatrix3x3;
+import org.ejml.dense.fixed.CommonOps_DDF3;
+
 public class BoxtubeKinematics {
     public static final double
-            BOXTUBE_PIVOT_TO_ARM_PIVOT_HORIZ = 11.79,
-            BOXTUBE_PIVOT_TO_ARM_PIVOT_VERT = -1.058; // everything in inches/radians
+            BOXTUBE_x = 11.79, BOXTUBE_y = -1.058,
+            ARM_x = 3.0, ARM_y = 0.5,
+            WRIST_x = 2.5, WRIST_y = -0.5,
+
+            Px = -5.5, Py = 4.0; // everything in inches/radians
+
+    public static DMatrix3x3 PIVOT1 = new DMatrix3x3(
+            1, 0, Px,
+            0, 1, Py,
+            0, 0, 1
+    );
+
+    public static DMatrix3x3 getBlockMatrix(double pivotAngle, double extension, double armAngle, double wristAngle) {
+        DMatrix3x3 result = new DMatrix3x3();
+
+        DMatrix3x3 transform = new DMatrix3x3(
+                1, 0, ARM_x,
+                0, 1, ARM_y + Math.sin(wristAngle) * WRIST_y,
+                0, 0, 1
+        );
+
+        DMatrix3x3 pivot2 = getPivot2(pivotAngle, extension, armAngle);
+
+        CommonOps_DDF3.mult(pivot2, transform, result);
+        return result;
+    }
+
+    public static DMatrix3x3 getPivot2(double pivotAngle, double extension, double armAngle) {
+        DMatrix3x3 result = new DMatrix3x3();
+
+        double cos = Math.cos(armAngle);
+        double sin = Math.sin(armAngle);
+        DMatrix3x3 extensionMatrix = new DMatrix3x3(
+                cos, -sin, BOXTUBE_x + extension,
+                sin, cos, BOXTUBE_y,
+                0, 0, 1
+        );
+
+        DMatrix3x3 p1 = getPivot1(pivotAngle);
+
+        CommonOps_DDF3.mult(p1, extensionMatrix, result);
+        return result;
+    }
+
+    public static DMatrix3x3 getPivot1(double pivotAngle){
+        DMatrix3x3 result = new DMatrix3x3();
+        double cos = Math.cos(pivotAngle);
+        double sin = Math.sin(pivotAngle);
+
+        DMatrix3x3 rotation = new DMatrix3x3(
+                cos, -sin, 0,
+                sin, cos, 0,
+                0, 0, 1
+        );
+
+        CommonOps_DDF3.mult(PIVOT1, rotation, result);
+        return result;
+    }
+
+    public static double getAngleFrom3x3 (DMatrix3x3 matrix) {
+        return Math.atan2(matrix.get(1, 0), matrix.get(0, 0));
+    }
+
+    public static Vector2d getVectorFrom3x3 (DMatrix3x3 matrix) {
+        return new Vector2d(matrix.get(0, 2), matrix.get(1, 2));
+    }
 }
--- TeamCode/src/test/java/JUnitTest.java
+++ TeamCode/src/test/java/JUnitTest.java
@@ -1,22 +1,81 @@
 import org.ejml.data.DMatrix3;
 import org.ejml.data.DMatrix3x3;
 import org.ejml.dense.fixed.CommonOps_DDF3;
+import org.firstinspires.ftc.teamcode.blucru.common.subsystems.boxtube.BoxtubeKinematics;
 import org.junit.*;
+
+import java.util.Arrays;
 
 public class JUnitTest {
     @Test
-    public void testCar() {
-        Assert.assertEquals(1, 1);
+    public void testBlockMatrix() {
+        doForwardKinematics(0, 0, 0, 0);
+        
+
+        Assert.assertEquals(4, 4);
+    }
+
+    public void doForwardKinematics(double pivotAngle, double extension, double armAngle, double wristAngle) {
+        long startTime, endTime;
+
+        System.out.println(Arrays.asList(pivotAngle, extension, armAngle, wristAngle));
+        startTime = System.nanoTime();
+        DMatrix3x3 res = BoxtubeKinematics.getBlockMatrix(pivotAngle, extension, armAngle, wristAngle);
+        endTime = System.nanoTime();
+        res.print();
+        System.out.println("Angle: " + BoxtubeKinematics.getAngleFrom3x3(res));
+        System.out.println("Time: " + (endTime - startTime) + " ns");
+        System.out.println();
     }
 
     @Test
-    public void testEJML() {
-        DMatrix3x3 A = new DMatrix3x3(1,2,3,
-                                       4,5,6,
-                                       7,8,9);
+    public void testOriginToPivot2() {
+        System.out.println("PI/2, 0, 0");
+        long startTime;
 
-        System.out.println(A);
-        System.out.println("det: " + CommonOps_DDF3.det(A));
+        startTime = System.nanoTime();
+        DMatrix3x3 res = BoxtubeKinematics.getPivot2(Math.PI/2,0, 0);
+        System.out.println("Time: " + (System.nanoTime() - startTime) + " ns");
+        res.print();
+        System.out.println(BoxtubeKinematics.getAngleFrom3x3(res));
+
+        System.out.println();
+
+        System.out.println("0, 0, PI/2");
+        startTime = System.nanoTime();
+        DMatrix3x3 res2 = BoxtubeKinematics.getPivot2(0,0, Math.PI/2);
+        System.out.println("Time: " + (System.nanoTime() - startTime) + " ns");
+        res2.print();
+        System.out.println(BoxtubeKinematics.getAngleFrom3x3(res2));
+
+        System.out.println();
+
+        System.out.println("1, 5, 1");
+        startTime = System.nanoTime();
+        DMatrix3x3 res3 = BoxtubeKinematics.getPivot2(1,5, 1);
+        System.out.println("Time: " + (System.nanoTime() - startTime) + " ns");
+        res3.print();
+        System.out.println(BoxtubeKinematics.getAngleFrom3x3(res3));
+
+        System.out.println();
+
+        System.out.println("0, 5, 2");
+        startTime = System.nanoTime();
+        DMatrix3x3 res4 = BoxtubeKinematics.getPivot2(0,5, 2);
+        System.out.println("Time: " + (System.nanoTime() - startTime) + " ns");
+        res4.print();
+        System.out.println(BoxtubeKinematics.getAngleFrom3x3(res4));
+
+        System.out.println();
+
+        Assert.assertEquals(4, 4);
+    }
+
+    @Test
+    public void testOriginToRotatedPivot1() {
+        DMatrix3x3 res = BoxtubeKinematics.getPivot1(1);
+        res.print();
+        System.out.println(BoxtubeKinematics.getAngleFrom3x3(res));
         Assert.assertEquals(4, 4);
     }
 
@@ -24,8 +83,8 @@ public class JUnitTest {
     public void testRotation() {
         double theta = Math.PI/2;
         DMatrix3x3 R = new DMatrix3x3(Math.cos(theta), -Math.sin(theta), 0,
-                                       Math.sin(theta), Math.cos(theta), 0,
-                                       0, 0, 1);
+                Math.sin(theta), Math.cos(theta), 0,
+                0, 0, 1);
         R.print();
 
         DMatrix3 v = new DMatrix3(1, 0, 1);
@@ -36,5 +95,21 @@ public class JUnitTest {
         result.print();
 
         Assert.assertEquals(4, 4);
+    }
+
+    @Test
+    public void testEJML() {
+        DMatrix3x3 A = new DMatrix3x3(1,2,3,
+                4,5,6,
+                7,8,9);
+
+        System.out.println(A);
+        System.out.println("det: " + CommonOps_DDF3.det(A));
+        Assert.assertEquals(4, 4);
+    }
+
+    @Test
+    public void testCar() {
+        Assert.assertEquals(1, 1);
     }
 }
