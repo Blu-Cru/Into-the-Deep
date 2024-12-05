0c42256e23e3fae2069aa33cf7fad190818a61ad
kevinndongg <kevin.dong.haonan@gmail.com>
2024-12-05 12:11:09 -0500

    testing 3x3 matrix

--- TeamCode/src/test/java/JUnitTest.java
+++ TeamCode/src/test/java/JUnitTest.java
@@ -1,3 +1,5 @@
+import org.ejml.data.DMatrix3x3;
+import org.ejml.dense.fixed.CommonOps_DDF3;
 import org.junit.*;
 
 public class JUnitTest {
@@ -8,6 +10,12 @@ public class JUnitTest {
 
     @Test
     public void testEJML() {
-
+        DMatrix3x3 A = new DMatrix3x3(1,2,3,
+                                       4,5,6,
+                                       7,8,9);
+//        SimpleMatrix A = new SimpleMatrix(2, 2);
+        System.out.println(A);
+        System.out.println("det: " + CommonOps_DDF3.det(A));
+        Assert.assertEquals(4, 4);
     }
 }
