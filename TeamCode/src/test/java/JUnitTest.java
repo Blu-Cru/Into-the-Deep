b90daf3432965adcd1849265b3893238c82bd410
kevinndongg <kevin.dong.haonan@gmail.com>
2024-12-05 12:49:51 -0500

    rotation working damnnn

--- TeamCode/src/test/java/JUnitTest.java
+++ TeamCode/src/test/java/JUnitTest.java
@@ -1,3 +1,4 @@
+import org.ejml.data.DMatrix3;
 import org.ejml.data.DMatrix3x3;
 import org.ejml.dense.fixed.CommonOps_DDF3;
 import org.junit.*;
@@ -13,9 +14,27 @@ public class JUnitTest {
         DMatrix3x3 A = new DMatrix3x3(1,2,3,
                                        4,5,6,
                                        7,8,9);
-//        SimpleMatrix A = new SimpleMatrix(2, 2);
+
         System.out.println(A);
         System.out.println("det: " + CommonOps_DDF3.det(A));
+        Assert.assertEquals(4, 4);
+    }
+
+    @Test
+    public void testRotation() {
+        double theta = Math.PI/2;
+        DMatrix3x3 R = new DMatrix3x3(Math.cos(theta), -Math.sin(theta), 0,
+                                       Math.sin(theta), Math.cos(theta), 0,
+                                       0, 0, 1);
+        R.print();
+
+        DMatrix3 v = new DMatrix3(1, 0, 1);
+        v.print();
+        DMatrix3 result = new DMatrix3();
+
+        CommonOps_DDF3.mult(R, v, result);
+        result.print();
+
         Assert.assertEquals(4, 4);
     }
 }
