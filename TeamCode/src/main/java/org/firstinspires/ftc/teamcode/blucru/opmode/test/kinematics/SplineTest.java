d68c72cf4ee8988ce633c1bc76ddd56966ccf7f5
kevinndongg <kevin.dong.haonan@gmail.com>
2024-12-13 12:16:10 -0500

    created spline test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmode/test/kinematics/SplineTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.blucru.opmode.test.kinematics;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.blucru.common.subsystems.boxtube.kinematics.BoxtubeSpline;
+import org.firstinspires.ftc.teamcode.blucru.opmode.BluLinearOpMode;
+
+@Config
+@TeleOp
+public class SplineTest extends BluLinearOpMode {
+    BoxtubeSpline spline;
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
+    }
+}
