37aaba5fbcf87f3f6199c4ffb76422f04dad9be4
kevinndongg <kevin.dong.haonan@gmail.com>
2024-12-19 08:14:08 -0500

    updated to change block angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/pathbase/specimen/SpecimenPreloadDepositSplinePath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/pathbase/specimen/SpecimenPreloadDepositSplinePath.java
@@ -17,9 +17,9 @@ public class SpecimenPreloadDepositSplinePath extends PIDPathBuilder {
         super();
         this.setPower(0.8)
                 .schedule(new BoxtubeSplineCommand(
-                        new Pose2d(6.4, 22.5, -0.3),
+                        new Pose2d(6.4, 25, 0.2),
                         -Math.PI,
-                        0.45
+                        0.6
                 ))
 //                .schedule(new SpecimenFrontCommand())
                 .addMappedPoint(7, -40, 90, 6)
@@ -28,16 +28,16 @@ public class SpecimenPreloadDepositSplinePath extends PIDPathBuilder {
                 .schedule(new SequentialCommandGroup(
                         new WaitCommand(350),
                         new BoxtubeSplineCommand(
-                                new Pose2d(6.4, 18, -0.3),
+                                new Pose2d(9, 18, 0.2),
                                 -Math.PI,
                                 0.2
                         ),
-                        new WaitCommand(300),
+                        new WaitCommand(200),
                         new ClampReleaseCommand(),
                         new WheelReverseCommand(),
 //                        new SpecimenFrontDunkCommand(),
                         new SpecimenFrontDunkRetractCommand()
                 ))
-                .waitMillis(600);
+                .waitMillis(500);
     }
 }
