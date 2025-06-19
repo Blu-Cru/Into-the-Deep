df9793844efc199c68f365f01a14db9e8499850b
kevinndongg <kevin.dong.haonan@gmail.com>
2025-06-19 01:06:36 -0400

    setup distance sensors math, realized ts is hard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/OrthogonalDistanceSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/OrthogonalDistanceSensors.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.blucru.common.subsystems;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.arcrobotics.ftclib.command.Subsystem;
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 
@@ -33,5 +34,23 @@ public class OrthogonalDistanceSensors implements BluSubsystem, Subsystem {
     public void telemetry(Telemetry telemetry) {
         telemetry.addData("Left Distance (in)", leftDistance);
         telemetry.addData("Right Distance (in)", rightDistance);
+    }
+
+    /*
+     class for localization using two distance sensors
+
+     this is assuming the sensors are only measuring the field walls, nothing else is in the way
+    */
+    static class OrthogonalDistanceSensorsPoseEstimation {
+        static final Pose2d
+                // positions of sensors on the robot (x is forward, y is left)
+                LEFT_SENSOR_ROBOT_POSE = new Pose2d(-7.0, 6.0, 3.0*Math.PI / 4.0),
+                RIGHT_SENSOR_ROBOT_POSE = new Pose2d(-7.0, -6.0, -3.0*Math.PI / 4.0);
+
+        static final double FIELD_WALL_OFFSET = 72.0; // inches from center of field
+
+        public static Pose2d getRobotPose (double leftDistance, double rightDistance, double robotHeading) {
+            return new Pose2d();
+        }
     }
 }
