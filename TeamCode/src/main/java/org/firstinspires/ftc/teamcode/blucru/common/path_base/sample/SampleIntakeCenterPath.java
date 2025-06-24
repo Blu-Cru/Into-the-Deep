b6ae6851b7dca11273082b10dcaa2d3459a13014
Deven Balaji <devenvbalaji@gmail.com>
2025-06-24 18:03:57 -0400

    updated sample auto commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/path_base/sample/SampleIntakeCenterPath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/path_base/sample/SampleIntakeCenterPath.java
@@ -8,30 +8,34 @@ import org.firstinspires.ftc.teamcode.blucru.common.command_base.boxtube.Extensi
 import org.firstinspires.ftc.teamcode.blucru.common.command_base.boxtube.PivotRetractCommand;
 import org.firstinspires.ftc.teamcode.blucru.common.command_base.end_effector.claw.ClawOpenCommand;
 import org.firstinspires.ftc.teamcode.blucru.common.command_base.end_effector.spin_wrist.SpinWristAngleCommand;
+import org.firstinspires.ftc.teamcode.blucru.common.command_base.end_effector.spin_wrist.SpinWristGlobalAngleCommand;
 import org.firstinspires.ftc.teamcode.blucru.common.command_base.end_effector.turret.TurretAngleCommand;
 import org.firstinspires.ftc.teamcode.blucru.common.command_base.intake.GrabCommand;
 import org.firstinspires.ftc.teamcode.blucru.common.command_base.intake.PreIntakeCommand;
 import org.firstinspires.ftc.teamcode.blucru.common.path.PIDPathBuilder;
+import org.firstinspires.ftc.teamcode.blucru.common.util.SampleOrientation;
 
 public class SampleIntakeCenterPath extends PIDPathBuilder {
     public SampleIntakeCenterPath() {
         super();
         this.setPower(0.7)
                 .addMappedPoint(-54, -45, 90, 4)
-                .schedule(
-                        new SequentialCommandGroup(
-                                new WaitCommand(150),
-                                new PivotRetractCommand(),
-                                new WaitCommand(300),
-                                new ClawOpenCommand(),
-                                new TurretAngleCommand(0.15),
-                                new ExtensionMotionProfileCommand(8),
-                                new WaitCommand(700),
-                                new PreIntakeCommand(),
-                                new WaitCommand(200),
-                                new GrabCommand(),
-                                new WaitCommand(300)
-                        )
-                ).waitMillis(4000);
+                .callback(() -> {
+                    new SequentialCommandGroup(
+                            new WaitCommand(150),
+                            new PivotRetractCommand(),
+                            new WaitCommand(300),
+                            new ClawOpenCommand(),
+                            new TurretAngleCommand(0.15),
+                            new SpinWristGlobalAngleCommand(SampleOrientation.VERTICAL),
+                            new ExtensionMotionProfileCommand(8),
+                            new WaitCommand(700),
+                            new PreIntakeCommand(),
+                            new WaitCommand(200),
+                            new GrabCommand(),
+                            new WaitCommand(300)
+                    ).schedule();
+                })
+                .waitMillis(4000);
     }
 }
